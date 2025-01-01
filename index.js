39c7eae2ce7095f6af6e66ba6b720d7f5dcfe4a0
harshagrawal909 <harshagrawal4256@gmail.com>
2025-01-01 21:45:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function sound(event){
 function makesound(key){
     switch (key) {
         case "w":
-            var tom1= new Audio("/sounds/tom-1.mp3");
+            var tom1= new Audio("sounds/tom-1.mp3");
             tom1.play();
             break;
 
